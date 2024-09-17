7e9c2935f7add5dfe849792db5e8a36daec0905b
alexandre-touret <alexandre-touret@users.noreply.github.com>
2024-09-17 11:28:48 +0000

    deploy: 642e1cc39cde5b91f21317c40476300ee2cbe87c

--- assets/slidev/DrawingPreview-CF3dMBAP.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-DhEtEDyj.js";import"../modules/vue-CS-TXVx1.js";import"../index-7SFlRjnz.js";import"../modules/shiki-CUSzc9fh.js";export{o as default};
---
+++ assets/slidev/default-8BTQKmWt.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-6soBvrIb.js";import{_ as r}from"../index-Dt1ALd6Y.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
