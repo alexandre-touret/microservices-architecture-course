900fa7d0b045320e26a5d8398c0a8e0270b92990
alexandre-touret <alexandre-touret@users.noreply.github.com>
2024-10-10 14:35:01 +0000

    deploy: 4c9fdefb3b43aefac90e96f410b77c11065f6140

---
+++ assets/slidev/DrawingPreview-7wcaUGCV.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-DM_kB4dW.js";import"../modules/vue-Cy8aIer2.js";import"../index-Dsy5wfHR.js";import"../modules/shiki-BcqVXy8B.js";export{o as default};
